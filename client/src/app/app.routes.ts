488f97cf4b9e8de940c33a09ad867f232c6b5199
thanh-31032004 <vanthanhnguyen3103@gmail.com>
2025-03-24 22:12:03 +0700

    add header footer

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ClientComponent } from './layout/client/client.component';
+import { HompageComponent } from './pages/hompage/hompage.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: ClientComponent
+        component: ClientComponent,
+        children: [
+            { path: '', component: HompageComponent },
+            // { path: 'child2', component: Child2Component },
+            // { path: '...', component: AnotherComponent },
+        ]
     }
 ];
--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isHomePage: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isHomePage = this.router.url === '/' || this.router.url === '/home';
+    });
+  }
 }
